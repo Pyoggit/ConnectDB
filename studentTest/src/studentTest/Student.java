51c826ad868cdd071a01986699f09b604b3c17c6
jangpyo <wkdvy122851@gmail.com>
2024-11-21 08:03:20 +0900

    [UPDATE]  총점,평균,등수 정보 추가

--- studentTest/src/studentTest/Student.java
+++ studentTest/src/studentTest/Student.java
@@ -1,26 +1,33 @@
 package studentTest;
 
 public class Student {
-	private int stuNum;
+	private int no; //------------pk
 	private String name;
 	private int kor;
 	private int eng;
 	private int mat;
+	private int total;
+	private int ave;
+	private int rank;
 	
-	public Student(int stuNum, String name, int kor, int eng, int mat) {
-		this.stuNum = stuNum;
+	public Student(int no, String name, int kor, int eng, int mat, int total, int ave, int rank) {
+		super();
+		this.no = no;
 		this.name = name;
 		this.kor = kor;
 		this.eng = eng;
 		this.mat = mat;
+		this.total = total;
+		this.ave = ave;
+		this.rank = rank;
 	}
 
-	public int getStuNum() {
-		return stuNum;
+	public int getNo() {
+		return no;
 	}
 
-	public void setStuNum(int stuNum) {
-		this.stuNum = stuNum;
+	public void setNo(int no) {
+		this.no = no;
 	}
 
 	public String getName() {
@@ -55,12 +62,36 @@ public class Student {
 		this.mat = mat;
 	}
 
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public int getAve() {
+		return ave;
+	}
+
+	public void setAve(int ave) {
+		this.ave = ave;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	
 	@Override
 	public String toString() {
-		return "Student [stuNum=" + stuNum + ", name=" + name + ", kor=" + kor + ", eng=" + eng + ", mat=" + mat + "]";
+		return "[학번 = " + no + ", 이름 = " + name + ", 국어점수 = " + kor + ", 영어점수 = " + eng + ", 수학점수 = " + mat + ", 총점 = "
+				+ total + ", 평균 = " + ave + ", 순위 = " + rank + "]";
 	}
-	
-	
 	
 	
 }
