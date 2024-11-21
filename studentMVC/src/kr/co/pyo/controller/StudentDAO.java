fe677a4546602a66b8a5f0a6dfea192aec7f120a
jangpyo <wkdvy122851@gmail.com>
2024-11-21 13:41:59 +0900

    [CREATE] Student 프로그램 Controller 분리

---
+++ studentMVC/src/kr/co/pyo/controller/StudentDAO.java
@@ -0,0 +1,154 @@
+package kr.co.pyo.controller;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import kr.co.pyo.StudentMVC;
+import kr.co.pyo.model.StudentVO;
+
+public class StudentDAO {
+	public static String selectSQL = "SELECT * FROM STUDENT";
+	public static String insertSQL = "INSERT INTO STUDENT(NO, NAME, KOR, ENG, MAT) VALUES(STUDENT_NO_SEQ.NEXTVAL, ?, ?, ?, ?)";
+	public static String updateSQL = "UPDATE STUDENT SET NAME = ?, KOR = ?, ENG = ?, MAT = ? WHERE NO = ? ";
+	public static String deleteSQL = "DELETE FROM STUDENT WHERE NO = ?";
+	public static String sortSQL = "SELECT * FROM STUDENT ORDER BY RANK";
+	public static String callRankProcSQL = "{call STUDENT_RANK_PROC()}";
+
+	// 출력
+	public static ArrayList<StudentVO> totalSelect() throws SQLException {
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		ArrayList<StudentVO> studentList = new ArrayList<StudentVO>();
+
+		con = DBUtility.dbCon();
+		stmt = con.createStatement();
+		rs = stmt.executeQuery(selectSQL);
+
+		while (rs.next()) {
+			int no = rs.getInt("NO");
+			String name = rs.getString("NAME");
+			int kor = rs.getInt("KOR");
+			int eng = rs.getInt("ENG");
+			int mat = rs.getInt("MAT");
+			int total = rs.getInt("TOTAL");
+			int ave = rs.getInt("AVE");
+			int rank = rs.getInt("RANK");
+
+			StudentVO stu = new StudentVO(no, name, kor, eng, mat, total, ave, rank);
+			studentList.add(stu);
+		}
+		// stuListPrint(studentList);
+		DBUtility.dbClose(con, stmt, rs);
+
+		return studentList;
+	}
+
+	// 입력
+	public static boolean studentInsert(StudentVO svo) throws SQLException {
+		// Conection
+		boolean successFlag = false;
+		Connection con = null;
+		CallableStatement cstmt = null;
+		PreparedStatement pstmt = null;
+
+		// 1 Load, 2. connect
+		con = DBUtility.dbCon();
+
+		pstmt = con.prepareStatement(insertSQL);
+		pstmt.setString(1, svo.getName());
+		pstmt.setInt(2, svo.getKor());
+		pstmt.setInt(3, svo.getEng());
+		pstmt.setInt(4, svo.getMat());
+
+		int result1 = pstmt.executeUpdate();
+		System.out.println((result1 != 0) ? "입력성공" : "입력실패");
+
+		cstmt = con.prepareCall(callRankProcSQL);
+		int result2 = cstmt.executeUpdate();
+		System.out.println((result2 != 0) ? "프로시저성공" : "프로시저실패");
+
+		DBUtility.dbClose(con, pstmt);
+		successFlag = (result1 != 0 && result2 != 0) ? true : false;
+
+		return successFlag;
+
+	}
+
+	// 수정
+	public static boolean studentUpdate(StudentVO svo) throws SQLException {
+		boolean successFlag = false;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		con = DBUtility.dbCon();
+		pstmt = con.prepareStatement(updateSQL);
+		pstmt.setString(1, svo.getName());
+		pstmt.setInt(2, svo.getKor());
+		pstmt.setInt(3, svo.getEng());
+		pstmt.setInt(4, svo.getMat());
+		pstmt.setInt(5, svo.getNo());
+
+		int result = pstmt.executeUpdate();
+
+		successFlag = (result != 0) ? true : false;
+
+		DBUtility.dbClose(con, pstmt);
+		return successFlag;
+	}
+
+	// 삭제
+	public static boolean studentDelete(StudentVO svo) throws SQLException {
+		boolean successFlag = false;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		con = DBUtility.dbCon();
+
+		pstmt = con.prepareStatement(deleteSQL);
+		pstmt.setInt(1, svo.getNo());
+		int result = pstmt.executeUpdate();
+
+		successFlag = (result != 0) ? true : false;
+
+		DBUtility.dbClose(con, pstmt);
+		return successFlag;
+	}
+
+	// 정렬
+	public static ArrayList<StudentVO> studentSort() throws SQLException {
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		ArrayList<StudentVO> studentList = new ArrayList<StudentVO>();
+
+		con = DBUtility.dbCon();
+		stmt = con.createStatement();
+		rs = stmt.executeQuery(selectSQL);
+
+		while (rs.next()) {
+			int no = rs.getInt("NO");
+			String name = rs.getString("NAME");
+			int kor = rs.getInt("KOR");
+			int eng = rs.getInt("ENG");
+			int mat = rs.getInt("MAT");
+			int total = rs.getInt("TOTAL");
+			int ave = rs.getInt("AVE");
+			int rank = rs.getInt("RANK");
+
+			StudentVO stu = new StudentVO(no, name, kor, eng, mat, total, ave, rank);
+			studentList.add(stu);
+		}
+
+		DBUtility.dbClose(con, stmt, rs);
+		return studentList;
+
+	}
+}
---
+++ studentMVC/src/kr/co/pyo/controller/StudentRegisterManager.java
@@ -0,0 +1,104 @@
+package kr.co.pyo.controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+import kr.co.pyo.StudentMVC;
+import kr.co.pyo.model.StudentVO;
+
+public class StudentRegisterManager {
+
+	public static Scanner sc = new Scanner(System.in);
+
+	// 출력
+	public static void totalSelectManager() throws SQLException {
+		ArrayList<StudentVO> studentList = new ArrayList<StudentVO>();
+		studentList = StudentDAO.totalSelect();
+		printStudentList(studentList);
+	}
+
+	// 입력
+	public static void insertManager() throws SQLException {
+
+		System.out.print("학생 이름을 입력하세요: ");
+		String name = sc.nextLine();
+		System.out.print("국어 점수를 입력하세요: ");
+		int kor = Integer.parseInt(sc.nextLine());
+		System.out.print("영어 점수를 입력하세요: ");
+		int eng = Integer.parseInt(sc.nextLine());
+		System.out.print("수학 점수를 입력하세요: ");
+		int mat = Integer.parseInt(sc.nextLine());
+
+		StudentVO studentVO = new StudentVO(name, kor, eng, mat);
+		boolean successFlag = StudentDAO.studentInsert(studentVO);
+
+		if (successFlag == true) {
+			System.out.println("입력처리 성공");
+		} else {
+			System.out.println("입력처리 실패");
+		}
+	}
+
+	// 수정
+	public static void updateManager() throws SQLException {
+		System.out.print("수정할 학생의 번호를 입력하세요: ");
+		int no = Integer.parseInt(sc.nextLine());
+		System.out.print("새로운 이름을 입력하세요: ");
+		String name = sc.nextLine();
+		System.out.print("새로운 국어 점수를 입력하세요: ");
+		int kor = Integer.parseInt(sc.nextLine());
+		System.out.print("새로운 영어 점수를 입력하세요: ");
+		int eng = Integer.parseInt(sc.nextLine());
+		System.out.print("새로운 수학 점수를 입력하세요: ");
+		int mat = Integer.parseInt(sc.nextLine());
+
+		StudentVO svo = new StudentVO(no, name, kor, eng, mat);
+		boolean successFlag = StudentDAO.studentUpdate(svo);
+
+		if (successFlag == true) {
+			System.out.println("입력처리 성공");
+		} else {
+			System.out.println("입력처리 실패");
+		}
+	}
+
+	// 삭제
+	public static void deleteManager() throws SQLException {
+		System.out.print("삭제할 학생 번호를 입력하세요: ");
+		int no = Integer.parseInt(sc.nextLine());
+		StudentVO svo = new StudentVO();
+		svo.setNo(no);
+		boolean successFlag = StudentDAO.studentDelete(svo);
+		if (successFlag == true) {
+			System.out.println("삭제처리 성공");
+		} else {
+			System.out.println("삭제처리 실패");
+		}
+
+	}
+
+	// 정렬
+	public static void sortManager() throws SQLException {
+		ArrayList<StudentVO> studentList = null;
+		studentList = StudentDAO.studentSort();
+		printStudentList(studentList);
+
+	}
+
+	// 전체 학생 리스트를 출력
+	public static void printStudentList(ArrayList<StudentVO> studentList) {
+		System.out.println("=============================");
+		for (StudentVO sv : studentList) {
+			System.out.println(sv.toString());
+		}
+		System.out.println("=============================");
+	}
+
+}
