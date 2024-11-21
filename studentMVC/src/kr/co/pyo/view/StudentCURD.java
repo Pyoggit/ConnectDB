5d436d59d5bce322d5886e0b1243ff76bbb9f7f6
jangpyo <wkdvy122851@gmail.com>
2024-11-21 13:44:50 +0900

    [CREATE] Student 프로그램 VIEW 분리 (Print Menu, CRUD Menu)

---
+++ studentMVC/src/kr/co/pyo/view/StudentCURD.java
@@ -0,0 +1,11 @@
+package kr.co.pyo.view;
+
+public interface StudentCURD {
+	//(출력, 입력, 수정, 삭제, 정렬, 종료) 메뉴
+	int PRINT = 1;
+	int INSERT = 2;
+	int UPDATE = 3;
+	int DELETE = 4;
+	int SORT = 5;
+	int EXIT = 6;
+}
