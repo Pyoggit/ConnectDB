bdb207b3f8b4cfcb64a18dd7b365a6cc9e67f5d3
jangpyo <wkdvy122851@gmail.com>
2024-11-21 17:08:31 +0900

    [UPDATE] String.format() 사용하여 프린트 메뉴 디자인

--- studentMVC/src/kr/co/pyo/view/StudentMenu.java
+++ studentMVC/src/kr/co/pyo/view/StudentMenu.java
@@ -3,15 +3,18 @@ package kr.co.pyo.view;
 public class StudentMenu {
 	// 프린트 메뉴
 	public static void printMenu() {
-		System.out.println("+++++++++++++++++++++");
-		System.out.println("1.학생정보출력");
-		System.out.println("2.학생정보입력");
-		System.out.println("3.학생정보수정");
-		System.out.println("4.학생정보삭제");
-		System.out.println("5.총점순서정렬");
-		System.out.println("6.종료");
-		System.out.println("+++++++++++++++++++++");
-		System.out.print(">>");
+	    System.out.println("┌──────────────────────────");
+	    System.out.println("│        학생 관리 시스템      ");
+	    System.out.println("├──────────────────────────");
+	    System.out.println("│ 1. 학생 정보 출력           ");
+	    System.out.println("│ 2. 학생 정보 입력           ");
+	    System.out.println("│ 3. 학생 정보 수정           ");
+	    System.out.println("│ 4. 학생 정보 삭제           ");
+	    System.out.println("│ 5. 총점 순서 정렬           ");
+	    System.out.println("│ 6. 종료                   ");
+	    System.out.println("└──────────────────────────");
+	    System.out.print(">> ");
 	}
+
 
 }
