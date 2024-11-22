6bfcf873d0f074323e82b1a3d5f8eb17255d270c
jangpyo <wkdvy122851@gmail.com>
2024-11-22 11:28:08 +0900

    [UPDATE] 프린트 메뉴 디자인 수정

--- studentMVC/src/kr/co/pyo/view/StudentMenu.java
+++ studentMVC/src/kr/co/pyo/view/StudentMenu.java
@@ -3,18 +3,13 @@ package kr.co.pyo.view;
 public class StudentMenu {
 	// 프린트 메뉴
 	public static void printMenu() {
-	    System.out.println("┌──────────────────────────");
-	    System.out.println("│        학생 관리 시스템      ");
-	    System.out.println("├──────────────────────────");
-	    System.out.println("│ 1. 학생 정보 출력           ");
-	    System.out.println("│ 2. 학생 정보 입력           ");
-	    System.out.println("│ 3. 학생 정보 수정           ");
-	    System.out.println("│ 4. 학생 정보 삭제           ");
-	    System.out.println("│ 5. 총점 순서 정렬           ");
-	    System.out.println("│ 6. 종료                   ");
-	    System.out.println("└──────────────────────────");
-	    System.out.print(">> ");
+	    System.out.println("─────────────────────────────────────────────────────────────────────────────────────");
+	    System.out.println("                              ─ 학생 성적 관리 시스템 ─                                   ");
+	    System.out.println("─────────────────────────────────────────────────────────────────────────────────────");
+	    System.out.println("1. 학생정보 출력 │ 2. 학생정보 입력 │ 3. 학생정보 수정 │ 4. 학생정보 삭제 │ 5. 성적순위 보기 │6. 종료 ");
+	    System.out.println("─────────────────────────────────────────────────────────────────────────────────────");
+	    System.out.print("실행할 메뉴의 번호를 입력하세요.\n>> ");
+	    
 	}
-
 
 }
